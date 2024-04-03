2b2720adc19dd3832b97db904792914ba84e02d0
Max Rukomoynikov <rukomoynikov@gmail.com>
2024-04-03 21:23:43 +0100

    Update finding correct command

--- sending_rubocop_report.sh
+++ sending_rubocop_report.sh
@@ -2,6 +2,16 @@ rm .rubocop.yml
 
 bundle exec rubocop -f json -o rubocop-report.json
 
+echo "----------------"
+
+echo $CODEMETRICS_TOKEN
+
+echo "----------------"
+
+echo $GITHUB_HEAD_REF
+
+echo "----------------"
+
 curl --location 'https://codemetrics.dev/reports' \
 --form 'report=@"./rubocop-report.json"' \
 --form 'tool="rubocop"' \
