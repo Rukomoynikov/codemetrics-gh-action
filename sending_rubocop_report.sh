554f684642e62f13b15b3b5158c18b335ca5fc18
Max Rukomoynikov <rukomoynikov@gmail.com>
2024-04-08 22:16:11 +0100

    Provide working directory

--- sending_rubocop_report.sh
+++ sending_rubocop_report.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
+
+if [ -n "$INPUT_WORKING_DIRECTORY" ]; then
+    # Check if the directory specified by the environment variable exists
+    if [ -d "$INPUT_WORKING_DIRECTORY" ]; then
+        cd "$INPUT_WORKING_DIRECTORY"
+        echo "Changed directory to: $INPUT_WORKING_DIRECTORY"
+    else
+        echo "Directory specified by 'INPUT_WORKING_DIRECTORY' does not exist: $INPUT_WORKING_DIRECTORY"
+    fi
+fi
+
 rm .rubocop.yml
 
 bundle exec rubocop -f json -o rubocop-report.json
-
-echo "----------------"
-
-echo $CODEMETRICS_TOKEN
-
-echo "----------------"
-
-echo $GITHUB_HEAD_REF
-
-echo "----------------"
 
 curl --location 'https://codemetrics.dev/reports' \
 --form 'report=@"./rubocop-report.json"' \
